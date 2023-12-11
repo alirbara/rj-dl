bedf20d203cd81e68f84f65c7628d4c54a10ba13
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-12-11 22:42:22 +0330

    feat: add prerequisites

--- bot/setup.sh
+++ bot/setup.sh
@@ -3,6 +3,11 @@
 function main() {
     # Write bot token into .env
     read -p "Enter the bot token: " token
+    
+    # Install prerequisites
+    sudo apt install nodejs
+    npm i -g pm2
+    pm2 startup
     echo "BOT_TOKEN=$token" > .env
 
     # Install dependencies
@@ -10,6 +15,7 @@ function main() {
 
     # Deploy using pm2
     pm2 start bot.js --name "rj-dl"
+    pm2 save
 
 }
 
