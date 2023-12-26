76030441a96cfc595c34e3a8e6c2118cbea31854
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-12-26 21:37:01 +0330

    fix: fix sync

--- bot/bot.js
+++ bot/bot.js
@@ -226,11 +226,11 @@ async function parseMessage(msg) {
 
   if (messageText.startsWith("https://")) {
     let url = messageText;
-    parseRequest(userId, url);
+    await parseRequest(userId, url);
   } 
     else if (detectUrl(messageText).length !== 0) {
-      detectUrl(messageText).forEach(url => {
-        parseRequest(userId, url)
+      detectUrl(messageText).forEach(async url => {
+        await parseRequest(userId, url)
       });
     }
   else {
